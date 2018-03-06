ad4a5d4d02fdfcb49da2b04b2410eb457cd64003
Domantas <domantasm@live.co.uk>
2018-03-06 14:35:14 +0000

    .

--- JavaFX_UI/src/javafx_ui/FXML_Homepage2Controller.java
+++ JavaFX_UI/src/javafx_ui/FXML_Homepage2Controller.java
@@ -13,7 +13,6 @@ import com.teamdev.jxmaps.javafx.MapView;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
@@ -60,8 +59,8 @@ public class FXML_Homepage2Controller extends Application  {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    //public static void main(String[] args) {
+    //    launch(args);
+    //}
 
 }
